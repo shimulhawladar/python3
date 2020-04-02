f2c80b1ac11152476b5481098624bcd61ff23c27
Shimul Hawladar <55784324+shimulhawladar@users.noreply.github.com>
2020-04-02 02:15:33 -0400

    Tuple_1

---
+++ tuple_tuple from the keyboard and print Sum and average.py
@@ -0,0 +1,15 @@
+# Write a program to take the tuple from the keyboard and print sum and average'
+
+numbers = input("Please type numbers like 1,2 : " )
+numbers = tuple(map(int,numbers.split(',')))
+total = 0
+
+print(numbers)
+
+for value in numbers:
+    total = total + value
+
+avg = total / len(numbers)
+
+print("The Sum of Natural Numbers from 1 to {0} =  {1}".format(numbers, total))
+print("Average of Natural Numbers from 1 to {0} =  {1}".format(numbers, avg))
