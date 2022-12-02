c59c807214868134c9c05e4aefc9ed71d4dfc1b0
Shimul Hawladar <55784324+shimulhawladar@users.noreply.github.com>
2022-12-02 02:54:18 -0500

    Create Fortune Game

---
+++ fortuneGame.py
@@ -0,0 +1,22 @@
+import random
+
+ActualNumber = random.randint(0,100)
+gameStatus = False
+
+turn = 5
+
+for x in range(1,turn+1):
+    guess = int(input("Guess a number between 0-100 : "))
+    if guess < ActualNumber :
+        print("Your guess is less than the actual number.")
+        print(f"Remaining { turn - x } out of 5 \n")
+    elif guess > ActualNumber :
+        print("Your guess is greater than the actual number.")
+        print(f"Remaining {turn - x} out of 5 \n")
+    else:
+        print("You Won!!! \nTotal tries " + w.toString)
+        gameStatus = True
+        break
+
+if gameStatus == False:
+    print(f"You loses the game. Actual Number is {ActualNumber}")
